6955b9f28bd7d279d19932dff51690f2fb1f3cf8
sistracia <156994197+sistracia@users.noreply.github.com>
2024-09-02 10:40:55 +0700

    Update `JWT` class

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,43 @@
 import { randomBytes, webcrypto } from "crypto";
 import * as jose from "jose";
 
-export class JWT {
-  #accessKey: webcrypto.CryptoKeyPair;
-  #refreshKey: webcrypto.CryptoKeyPair;
-  #issuer: string;
-  #subject: string;
-  #audience: string;
-  #edDSA = "EdDSA";
+export type Alg =
+  | "EdDSA"
+  | "ES256"
+  | "ES256K"
+  | "ES384"
+  | "ES512"
+  | "HS256"
+  | "HS384"
+  | "HS512"
+  | "PS256"
+  | "PS384"
+  | "PS512"
+  | "RS256"
+  | "RS384"
+  | "RS512";
 
-  constructor(
-    accessKey: webcrypto.CryptoKeyPair,
-    refreshKey: webcrypto.CryptoKeyPair,
-    issuer: string,
-    subject: string,
-    audience: string,
-  ) {
-    this.#accessKey = accessKey;
-    this.#refreshKey = refreshKey;
+export class CompactJWS {
+  #issuer: string;
+  #audience: string | string[];
+  #alg: Alg;
+
+  constructor(alg: Alg, issuer: string, audience: string | string[]) {
+    this.#alg = alg;
     this.#issuer = issuer;
-    this.#subject = subject;
     this.#audience = audience;
   }
 
-  async Sign(payload?: object): Promise<[string, string]> {
+  async Sign(
+    accessPrivateKey: webcrypto.CryptoKey,
+    refreshPrivateKey: webcrypto.CryptoKey,
+    payload?: object,
+  ): Promise<[string, string]> {
     const accessBuffer = randomBytes(32);
     const accessRandId = accessBuffer.toString("hex");
 
     const accessClaims: jose.JWTPayload = {
       iss: this.#issuer,
-      sub: this.#subject,
       aud: this.#audience,
       exp: Math.floor((Date.now() + 1 * 60 * 60 * 1000) / 1000), // One hour from now
       nbf: Math.floor(Date.now() / 1000),
@@ -39,15 +47,14 @@ export class JWT {
     };
 
     const accessToken = await new jose.SignJWT(accessClaims)
-      .setProtectedHeader({ alg: this.#edDSA })
-      .sign(this.#accessKey.privateKey);
+      .setProtectedHeader({ alg: this.#alg })
+      .sign(accessPrivateKey);
 
     const refreshBuffer = randomBytes(32);
     const refreshRandId = refreshBuffer.toString("hex");
 
     const refreshClaims: jose.JWTPayload = {
       iss: this.#issuer,
-      sub: this.#subject,
       aud: this.#audience,
       exp: Math.floor((Date.now() + 24 * 60 * 60 * 1000) / 1000), // 24 hour from now
       nbf: Math.floor((Date.now() + 59 * 60 * 1000) / 1000), // 59 minute
@@ -57,9 +64,51 @@ export class JWT {
     };
 
     const refreshToken = await new jose.SignJWT(refreshClaims)
-      .setProtectedHeader({ alg: this.#edDSA })
-      .sign(this.#refreshKey.privateKey);
+      .setProtectedHeader({ alg: this.#alg })
+      .sign(refreshPrivateKey);
 
     return [accessToken, refreshToken];
+  }
+
+  async VerifyAccessToken(
+    token: string,
+    accessPublicKey: webcrypto.CryptoKey,
+  ): Promise<jose.JWTPayload> {
+    const jwtResult = await jose.jwtVerify(token, accessPublicKey, {
+      audience: this.#audience,
+      currentDate: new Date(),
+      issuer: this.#issuer,
+    });
+
+    const jwtPayload = jwtResult.payload;
+    if (!jwtPayload.jti) {
+      throw new Error("token claims invalid");
+    }
+
+    return jwtPayload;
+  }
+
+  async VerifyRefreshToken(
+    token: string,
+    refreshPublicKey: webcrypto.CryptoKey,
+  ): Promise<jose.JWTPayload> {
+    const jwtResult = await jose.jwtVerify(token, refreshPublicKey, {
+      audience: this.#audience,
+      currentDate: new Date(),
+      issuer: this.#issuer,
+    });
+
+    const jwtPayload = jwtResult.payload;
+    if (!jwtPayload.jti) {
+      throw new Error("token claims invalid");
+    }
+
+    return jwtPayload;
+  }
+}
+
+export class JWT extends CompactJWS {
+  constructor(issuer: string, audience: string) {
+    super("EdDSA", issuer, audience);
   }
 }
